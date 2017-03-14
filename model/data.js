6edda7c71a881b20d93b991bbdff5b8f302b7dee
NAM\pdong008 <peter.dong@pwc.com>
2017-03-14 14:39:14 +0800

    js module

---
+++ model/data.js
@@ -0,0 +1,15 @@
+define(function() {
+    return [{
+            name: "google"
+        },
+        {
+            name: "facebook"
+        },
+        {
+            name: "yahoo"
+        },
+        {
+            name: "twitter"
+        }
+    ];
+})
